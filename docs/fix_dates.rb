7bb79017dd8be4c16c350c11af3ea36307d624fb
Gadi Aleksandrowicz <gadial@gmail.com>
2019-11-26 17:03:08 +0200

    Moved CM to rely on CDN for dojo

--- docs/fix_dates.rb
+++ docs/fix_dates.rb
@@ -1,9 +1,6 @@
 old_data = {}
-#File.read('wordpress.2019-06-24.xml').scan(/<link>https:\/\/gadial.net\/(\d\d\d\d)\/(\d\d)\/(\d\d)\/(.*)\/<\/link>/) {old_data[$4] = "#{$1}-#{$2}-#{$3}"}
-puts Dir.glob("docs/**/index.html")
-
-# .join(" ").scan(/(\d\d\d\d-\d\d-\d\d)-(.*?)\.md/) do
-#     if old_data[$2] != $1
-#         puts "#{$2}: #{$1}"
-#     end
-#end
+File.read('wordpress.2019-06-24.xml').scan(/<link>https:\/\/gadial.net\/(\d\d\d\d)\/(\d\d)\/(\d\d)\/(.*)\/<\/link>/) {old_data[$4] = "#{$1}-#{$2}-#{$3}"}
+Dir.glob("docs/**/index.html").join(" ").scan(/(\d\d\d\d)\/(\d\d)\/(\d\d)\/(.*?)\/index.html/) do
+    date = "#{$1}-#{$2}-#{$3}"
+    puts "#{$4}: #{date}" if old_data[$4] != date
+end
